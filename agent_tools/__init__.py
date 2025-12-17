18144f076701559d7e046507d57faac0a0386fbe
TunixR <antoniorodriguezruiz2002@gmail.com>
2025-12-17 12:17:01 +0100

    chore: Remove unused tools

--- agent_tools/__init__.py
+++ agent_tools/__init__.py
@@ -1,10 +1,7 @@
-from .database import register_plan, register_solution
 from .image import image_to_base64, take_screenshot
 from .utils import compute_continuation_activity
 
 __all__ = [
-    "register_plan",
-    "register_solution",
     "image_to_base64",
     "take_screenshot",
     "compute_continuation_activity",
