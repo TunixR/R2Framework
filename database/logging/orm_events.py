0561cb52553a486bc8719147591a304031ce4c6e
TunixR <antoniorodriguezruiz2002@gmail.com>
2026-01-20 10:13:10 +0100

    fix: Huge fucking skill issue

--- database/logging/orm_events.py
+++ database/logging/orm_events.py
@@ -1,4 +1,4 @@
-from sqlalchemy import event
+from sqlalchemy import Connection, event
 from sqlmodel import (
     Session,
     select,
@@ -8,38 +8,36 @@ from database.logging.models import GUITrace, SubAgentTrace
 from s3.utils import S3Client
 
 
-@event.listens_for(Session, "before_flush")
-def _validate_sub_agent_trace(
-    session: Session,
-    _,
-    _2,
-) -> None:
-    for obj in session.new:
-        if obj.parent_trace_id == obj.child_trace_id:
-            raise ValueError("A trace cannot be a sub-trace of itself.")
+@event.listens_for(SubAgentTrace, "before_insert")
+@event.listens_for(SubAgentTrace, "before_update")
+def _validate_sub_agent_trace(_, connection: Connection, target: SubAgentTrace) -> None:
+    if target.parent_trace_id == target.child_trace_id:
+        raise ValueError("A trace cannot be a sub-trace of itself.")
 
-        if session.exec(
-            select(SubAgentTrace).where(
-                SubAgentTrace.child_trace_id == obj.child_trace_id
-            )
-        ).first():
-            raise ValueError("One trace cannot have multiple parent traces.")
+    session = Session(bind=connection)
 
-        if session.exec(
-            select(SubAgentTrace).where(
-                (SubAgentTrace.parent_trace_id == obj.parent_trace_id)
-                & (SubAgentTrace.child_trace_id == obj.child_trace_id)
-            )
-        ).first():
-            raise ValueError("This sub-trace relationship already exists.")
+    if session.exec(
+        select(SubAgentTrace).where(
+            SubAgentTrace.child_trace_id == target.child_trace_id
+        )
+    ).first():
+        raise ValueError("One trace cannot have multiple parent traces.")
 
-        if session.exec(
-            select(SubAgentTrace).where(
-                (SubAgentTrace.child_trace_id == obj.parent_trace_id)
-                & (SubAgentTrace.parent_trace_id == obj.child_trace_id)
-            )
-        ).first():
-            raise ValueError("Circular sub-trace relationships are not allowed.")
+    if session.exec(
+        select(SubAgentTrace).where(
+            (SubAgentTrace.parent_trace_id == target.parent_trace_id)
+            & (SubAgentTrace.child_trace_id == target.child_trace_id)
+        )
+    ).first():
+        raise ValueError("This sub-trace relationship already exists.")
+
+    if session.exec(
+        select(SubAgentTrace).where(
+            (SubAgentTrace.child_trace_id == target.parent_trace_id)
+            & (SubAgentTrace.parent_trace_id == target.child_trace_id)
+        )
+    ).first():
+        raise ValueError("Circular sub-trace relationships are not allowed.")
 
 
 @event.listens_for(GUITrace, "before_delete")
